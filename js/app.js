c863970465ef465944c5578347afe10a3d6325bf
jlorena13 <Skaditm@gmail.com>
2021-04-28 16:19:11 -0400

    Revert "not completely working, but issue completely resolved"
    This reverts commit 7cef2aeedf547d6604ea652d00cf01ff754e2c40.

--- js/app.js
+++ js/app.js
@@ -1,29 +1,16 @@
 //Navigation Bar data
 
-function justafunction() {
-  const datnavs = document.querySelectorAll("[data-nav]");
+const datnavs = document.querySelectorAll(".data-nav");
 
-  console.log("Acquired datanavs");
-  console.log(datnavs);
 
-  let unorderedList = document.createElement("ul");
-
-  for (let datnav of datnavs) {
-    let createAnchor = document.createElement("a");
-    createAnchor.setAttribute("href", "#" + datnavs.id);
+for (let datnav of datnavs) {
+    const unorderedList = document.createElement("ul");
+    const crateAnchor = document.createElement("a");
+    createAnchor.setAttribute("#href", "# + datnavs.id");
     createAnchor.className = "page-links";
     createAnchor.textContent = datnav.getAttribute("data-nav");
-    unorderedList.appendChild(createAnchor);
-    //unorderedList.appendChild(ul);
-  }
-
-  document.body.appendChild(unorderedList);
-
-  console.log("list complete");
-  console.log(unorderedList);
+    ul.appendChild(createAnchor);
+    unorderedList.appendChild(ul);
 }
 
-window.onload = function(){
-    console.log("About to run function");
-    justafunction();
-  };
+
